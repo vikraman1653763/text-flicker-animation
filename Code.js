3e1e38ca08c68ceac02fbd4ecf6a94e5393aa283
VikramaN <133756180+vikraman1653763@users.noreply.github.com>
2025-06-04 16:32:46 +0530

    04-06-2025

---
+++ Code.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { motion } from "framer-motion";
+
+const getRandomDelay = () => Math.random();
+
+const BlinkingText = ({ text, className }) => {
+  return (
+    <p className={className}>
+      {text.split("").map((char, index) => (
+        <motion.span
+          key={index}
+          initial={{ opacity: 1 }}
+          whileInView={{ opacity: [0, 0.3, 0.5, 0.8, 1] }}
+          transition={{
+            duration: 1,
+            delay: getRandomDelay(),
+            repeat: 1,
+            ease: "linear",
+          }}
+          viewport={{ once: true }}
+          style={{
+            display: "inline-block",
+            whiteSpace: "pre",
+          }}
+        >
+          {char === " " ? "\u00A0" : char}
+        </motion.span>
+      ))}
+    </p>
+  );
+};
+
+export default BlinkingText;
